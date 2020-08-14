51e1195a35e59a715458412cde879860a3a28177
Shailesh Patel <shailesh001@mac.com>
2020-08-14 16:54:28 +0100

    Initial Commit

---
+++ Test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test2
+//
+//  Created by Shailesh Patel on 14/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Shailesh Patel on 14/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
